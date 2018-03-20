df46e846242a6ccccf13967bf823f728ddb17570
shkyung <emiliababy88@gmail.com>
2018-03-20 20:44:59 +0900

    sunny's second commit

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -10,12 +10,19 @@ class TodoList extends React.Component {
 
   onClickPlusButton = (e) => {
     console.log(e);
+    //this.state.input
+
+  }
+
+  onChangeInput = (e) => {
+  	console.log(e.target.value);
+  	this.setState( {input: e.target.value} );
   }
 
   render() {
     return (
       <div>
-        <input />
+        <input onChange={this.onChangeInput} value={this.state.input}/>
         <button onClick={this.onClickPlusButton}>+</button>
         <ul>
           <li>{this.state.input}</li>
