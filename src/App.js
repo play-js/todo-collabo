6c8627c6636d4332edd8b8475d867863aec15ad5
Taesu Hyeon <qvil1127@gmail.com>
2018-03-27 20:54:11 +0900

    class 내부 private, public 구현

--- src/App.js
+++ src/App.js
@@ -1,16 +1,19 @@
-import React, { Component } from 'react';
-import TodoList from './components/TodoList';
+import React, { Component } from "react";
+import TodoList from "./components/TodoList";
 
+var md = new TodoList();
+console.log(md.newState);
+console.log(md.getState());
 class App extends Component {
-
   render() {
     return (
       <div className="App">
         <TodoList />
+
+        {console.log(TodoList.newState)}
       </div>
     );
   }
 }
 
 export default App;
- 
--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,36 +1,40 @@
-import React from 'react';
+import React from "react";
+var newState;
 
 class TodoList extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
       input: "Play JS",
-      todos: [],
+      todos: []
     };
-
-    this.newState = [...this.state.todos];
   }
 
-  onClickPlusButton = (e) => {
+  getState = () => newState;
+
+  onClickPlusButton = e => {
     //let newState = [...this.state.todos];
 
-    this.newState.push(this.state.input);
-    this.setState({todos: this.newState});
-    
+    //this.newState.push(this.state.input);
+    this.setState({ todos: this.newState });
+
+    this.setState({ input: "" });
+    // DOM.focus();
+  };
+
+  onClickMinusButton = e => {
+    this.newState = this.newState.filter(
+      (data, i) => +e.target.getAttribute("index") !== i
+    );
+
+    this.setState({ todos: this.newState });
+
     console.log(this.state.todos);
-  }
+  };
 
-  onClickMinusButton = (e) => {
-    this.newState = this.newState.filter((data, i) => +e.target.getAttribute("index") !== i);
-
-    this.setState({todos: this.newState});
-
-    console.log(this.state.todos);
-  }
-
-  onChangeInput = (e) => {
+  onChangeInput = e => {
     this.setState({ input: e.target.value });
-  }
+  };
 
   render() {
     return (
@@ -38,13 +42,17 @@ class TodoList extends React.Component {
         <input onChange={this.onChangeInput} value={this.state.input} />
         <button onClick={this.onClickPlusButton}>+</button>
         <ul>
-          {
-            // this.state.todos.map(function(data, index) {
-            //   return <li key={index}>{data}</li>
-            // })
-            this.state.todos.map((data, index) => 
-              <li key={index}>{data}<button onClick={this.onClickMinusButton} index={index}>-</button></li>)
-          }
+          {// this.state.todos.map(function(data, index) {
+          //   return <li key={index}>{data}</li>
+          // })
+          this.state.todos.map((data, index) => (
+            <li key={index}>
+              {data}
+              <button onClick={this.onClickMinusButton} index={index}>
+                -
+              </button>
+            </li>
+          ))}
         </ul>
       </div>
     );
